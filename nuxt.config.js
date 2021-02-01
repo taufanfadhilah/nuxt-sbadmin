243b2834459ee55530307e2cf1493e70c40664e9
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-02-01 13:18:17 +0700

    install template for dashboard

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,27 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+      },
+    ],
+    script: [
+      { src: '/template/vendor/jquery/jquery.min.js' },
+      { src: '/template/vendor/bootstrap/js/bootstrap.bundle.min.js' },
+      { src: '/template/vendor/jquery-easing/jquery.easing.min.js' },
+      { src: '/template/js/sb-admin-2.min.js' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    '@/assets/template/vendor/fontawesome-free/css/all.min.css',
+    '@/assets/template/css/sb-admin-2.min.css',
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
